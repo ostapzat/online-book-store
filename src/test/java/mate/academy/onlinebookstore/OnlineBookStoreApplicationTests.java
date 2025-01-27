1fc885c0b55c6cccae2c22f23d0e6c92b40debaf
Ostap <ostzat@gmail.com>
2025-01-27 16:57:10 +0100

    fixed checkstyle

--- src/test/java/mate/academy/onlinebookstore/OnlineBookStoreApplicationTests.java
+++ src/test/java/mate/academy/onlinebookstore/OnlineBookStoreApplicationTests.java
@@ -6,14 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OnlineBookStoreApplicationTests {
 
-<<<<<<< HEAD
-	@Test
-	void contextLoads() {
-	}
-=======
     @Test
     void contextLoads() {
     }
->>>>>>> origin/main
 
 }
